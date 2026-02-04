1dce9aceb34650607687f28a1854d936b50d94cb
garen2002 <ernur2666@gmail.com>
2026-02-04 21:31:10 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,18 +1,20 @@
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('study-app').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html'
-      ]);
-    })
+const cacheName = 'studyflow-cache-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
